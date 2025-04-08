23adb11da0ba6a263aa503d15367a9e2fbf6f833
Jonathan Sheffer <j.sheffer747@gmail.com>
2025-04-08 23:03:45 +0300

    added functions to binarySet almost finished! will finish tomorrow

--- Ex01_01/BinaryNumbersSet.cs
+++ Ex01_01/BinaryNumbersSet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Ex01_01
 {
@@ -6,7 +7,7 @@ namespace Ex01_01
     {
         private const int k_LengthOfSet = 4;
         private BinaryNumber[] m_BinaryNumberArray;
-
+        private int m_NumOfOnes = 0;
         public BinaryNumbersSet()
         {
             m_BinaryNumberArray = new BinaryNumber[k_LengthOfSet];
@@ -14,10 +15,11 @@ namespace Ex01_01
             {
                 m_BinaryNumberArray[i] = new BinaryNumber();
                 m_BinaryNumberArray[i].GetBinaryNum();
+                m_NumOfOnes += m_BinaryNumberArray[i].GetNumberOfOnes();
             }
         }
-
-        public int binarySetAverage()
+        
+        public int BinarySetAverage()
         {
             int sum = 0;
             for(int i = 0; i < k_LengthOfSet; i++)
@@ -26,6 +28,38 @@ namespace Ex01_01
             }
 
             return sum / k_LengthOfSet;
+        }
+
+        public BinaryNumber GetLongestSeriesOfOnes()
+        {
+            int maxIndex = 0;
+            int max = m_BinaryNumberArray[0].GetLengthOfLongestSeriesOfOnes();
+            int[] arrayOfSeriesOfOnes = new int[k_LengthOfSet];
+            for(int i = 0; i < k_LengthOfSet; i++)
+            {
+                if(m_BinaryNumberArray[i].GetLengthOfLongestSeriesOfOnes() > max)
+                {
+                    max = m_BinaryNumberArray[i].GetLengthOfLongestSeriesOfOnes();
+                    maxIndex = i;
+                }
+            }
+
+            return m_BinaryNumberArray[maxIndex];
+        }
+
+        public BinaryNumber GetLargestNumOfOnes()
+        {
+            int maxIndex = 0;
+            int max = m_BinaryNumberArray[0].GetNumberOfOnes();
+            for(int i = 0; i < k_LengthOfSet; i++)
+            {
+                if(m_BinaryNumberArray[i].GetNumberOfOnes() > max)
+                {
+                    max = m_BinaryNumberArray[i].GetNumberOfOnes();
+                    maxIndex = i;
+                }
+            }
+            return m_BinaryNumberArray[maxIndex];
         }
     }
 }
--- Ex01_01/Program.cs
+++ Ex01_01/Program.cs
@@ -4,17 +4,17 @@ namespace Ex01_01
 {
     class Program
     {
-        static void Main()
-        {
-            //BinaryNumber b1 = new BinaryNumber();
-            //b1.GetBinaryNumberFromUser();
-            //Console.WriteLine(
-            //    "Binary Number: " + b1.GetBinaryNum() +
-            //    ", Decimal Value: " + b1.GetBinaryNumInDecimal() +
-            //    ", Changes Between 0 and 1: " + b1.GetNumberOfChangesBetween0And1() +
-            //    ", Number of Ones: " + b1.GetNumberOfOnes() +
-            //    ", Longest Series of Ones: " + b1.GetLengthOfLongestSeriesOfOnes()
-            //);
-        }
+        // static void Main()
+        // {
+        //     BinaryNumber b1 = new BinaryNumber();
+        //     b1.GetBinaryNumberFromUser();
+        //     Console.WriteLine(
+        //         "Binary Number: " + b1.GetBinaryNum() +
+        //         ", Decimal Value: " + b1.GetBinaryNumInDecimal() +
+        //         ", Changes Between 0 and 1: " + b1.GetNumberOfChangesBetween0And1() +
+        //         ", Number of Ones: " + b1.GetNumberOfOnes() +
+        //         ", Longest Series of Ones: " + b1.GetLengthOfLongestSeriesOfOnes()  
+        //     );
+        // }
     }
 }
