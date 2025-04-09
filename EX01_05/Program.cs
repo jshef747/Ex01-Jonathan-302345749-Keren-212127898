667d82dd8f8a7c3280392d3aeaa6d8039c890588
Jonathan Sheffer <j.sheffer747@gmail.com>
2025-04-09 13:11:52 +0300

    finished with 5 still needs running tests

---
+++ EX01_05/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EX01_05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Number n1 = new Number();
+            n1.printNumberDetails();
+        }
+    }
+}
