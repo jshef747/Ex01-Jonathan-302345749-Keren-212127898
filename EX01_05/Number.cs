ccf57019b9194ff5e91dd934dfb6fbc9ede8c671
Jonathan Sheffer <j.sheffer747@gmail.com>
2025-04-12 07:34:29 +0300

    deleted the array used in 5

--- EX01_05/Number.cs
+++ EX01_05/Number.cs
@@ -104,37 +104,43 @@ namespace EX01_05
 
         private int getTheDigitThatAppearsTheMostAndItsNumberOfAppearances(out int i_NumberOfAppearences)
         {
-            int[] digitCount = new int[10];
-            int dividedNumber = m_Number;
+            int maxNumberOfAppearences = 0;
+            int maxDigitThatAppears = m_Number % 10;
+            int currentNumberOfAppearences = 0;
+            int number = m_Number;
+            int numberChecker = m_Number;
             for(int i = 0; i < k_NumberLength; i++)
             {
-                int divideBy = (int)Math.Pow(10, k_NumberLength - 1 - i);
-                int currentDigit = dividedNumber / divideBy;
-                dividedNumber = dividedNumber % divideBy;
-                digitCount[currentDigit]++;
-            }
-            int digitWithMostNumberOfAppearances = 0;
-            i_NumberOfAppearences = digitCount[digitWithMostNumberOfAppearances];
-            for(int i = 1; i < 10; i++)
-            {
-                int currentAppearances = digitCount[i];
-                if(currentAppearances > digitCount[digitWithMostNumberOfAppearances])
+                int currentDigit = number % 10;
+                numberChecker = number;
+                for (int j = 0; j < k_NumberLength; j++)
                 {
-                    digitWithMostNumberOfAppearances = i;
-                    i_NumberOfAppearences = digitCount[digitWithMostNumberOfAppearances];
+                    if(numberChecker % 10 == currentDigit)
+                    {
+                        currentNumberOfAppearences++;
+                    }
+                    numberChecker /= 10;
                 }
+
+                if(currentNumberOfAppearences > maxNumberOfAppearences)
+                {
+                    maxNumberOfAppearences = currentNumberOfAppearences;
+                    maxDigitThatAppears = currentDigit;
+                }
+                number /= 10;
+                currentNumberOfAppearences = 0;
             }
-            return digitWithMostNumberOfAppearances;
+            i_NumberOfAppearences = maxNumberOfAppearences;
+            return maxDigitThatAppears;
         }
 
         public void PrintNumberDetails()
         {
-            int digitWithMostNumberOfAppearances;
             Console.WriteLine(string.Format(@"Number Details:
 the left digit: {0} number of digits smaller than it: {1}
 number of digits divided by 3 without remainder: {2}
 difference between largest and smallest digit: {3}
-the digit with the most appearances: {4} with {5} appearances", m_Number / (int) Math.Pow(10, k_NumberLength - 1), numberOfDigitsSmallerThenTheFirstDigit(),numberOfDigitsDividedBy3WithoutRemainder(),differenceBetweenLargestAndSmallestDigit(),getTheDigitThatAppearsTheMostAndItsNumberOfAppearances(out digitWithMostNumberOfAppearances), digitWithMostNumberOfAppearances));
+the digit with the most appearances: {4} with {5} appearances", m_Number / (int) Math.Pow(10, k_NumberLength - 1), numberOfDigitsSmallerThenTheFirstDigit(),numberOfDigitsDividedBy3WithoutRemainder(),differenceBetweenLargestAndSmallestDigit(),getTheDigitThatAppearsTheMostAndItsNumberOfAppearances(out int digitWithMostNumberOfAppearances), digitWithMostNumberOfAppearances));
         }
     }
 }
