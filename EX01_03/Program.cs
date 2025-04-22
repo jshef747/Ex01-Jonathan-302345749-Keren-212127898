56799b548e2971ca9a8298531aee88a4916edb39
KerenStoller <keren123stoller@gmail.com>
2025-04-22 16:00:28 +0300

    merged the classes in ex02 and ex03

--- EX01_03/Program.cs
+++ EX01_03/Program.cs
@@ -1,4 +1,5 @@
-﻿using Ex01_02;
+﻿using System;
+using Ex01_02;
 
 namespace EX01_03
 {
@@ -6,7 +7,22 @@ namespace EX01_03
     {
         static void Main() 
         {
-            NumbersTree.PrintNumberTree(DynamicNumberTree.GetHeightFromUser());
+            Ex01_02.Program.PrintNumberTree(GetHeightFromUser());
+        }
+
+        const int k_MinimumHeightForTree = 4;
+        const int k_MaximumHeightForTree = 15;
+
+        public static int GetHeightFromUser()
+        {
+            Console.WriteLine(string.Format("Enter a number between {0} and {1}:", k_MinimumHeightForTree, k_MaximumHeightForTree));
+
+            int userInputInt;
+            while (!int.TryParse(Console.ReadLine(), out userInputInt) || userInputInt < k_MinimumHeightForTree || userInputInt > k_MaximumHeightForTree)
+            {
+                Console.WriteLine(string.Format("Invalid input! Please enter a number between {0} and {1}:", k_MinimumHeightForTree, k_MaximumHeightForTree));
+            }
+            return userInputInt;
         }
     }
 }
